6bb97348a1ae05e07cd3774e5e0d9197c1f79bec
patrickchennn <patrick.chen31@gmail.com>
2022-07-02 13:57:02 +0700

    initial uploads

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
